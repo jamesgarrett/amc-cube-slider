24dc89eb19c595e891b86a40bfde8d595271cc5b
jamesgarrett <jgsibinga@gmail.com>
2020-03-20 12:53:00 -0400

    update gitignore to include builds in git repo

---
+++ build/precache-manifest.8ec425f5bf500b76928c73dd7ad5127f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b863bcd368afb568fa9979eb4311df1",
+    "url": "/amc-loader-protoype/index.html"
+  },
+  {
+    "revision": "bea5502ede89d0de03c7",
+    "url": "/amc-loader-protoype/static/css/main.d6daa3c8.chunk.css"
+  },
+  {
+    "revision": "2f29a6c4e25bb34d0968",
+    "url": "/amc-loader-protoype/static/js/2.b22cca78.chunk.js"
+  },
+  {
+    "revision": "bea5502ede89d0de03c7",
+    "url": "/amc-loader-protoype/static/js/main.73fe9417.chunk.js"
+  },
+  {
+    "revision": "d953f723b9505899f978",
+    "url": "/amc-loader-protoype/static/js/runtime~main.371a5d51.js"
+  }
+]);
